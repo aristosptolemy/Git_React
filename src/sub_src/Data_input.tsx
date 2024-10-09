5c9b8fec14c7452004a4d61cbc12fd430103e11f
ARISTOS <storm1legendaryhero@gmail.com>
2024-10-10 08:14:37 +0900

    css

--- src/sub_src/Data_input.tsx
+++ src/sub_src/Data_input.tsx
@@ -28,41 +28,43 @@ export default function DataInput({ setCurrentPage }: SettingProps) {
   };
 
   return(
-    <div className="set">
-      <div className="detail">
-        <table>
-          <tbody>
-            <tr>
-              <th scope='row'>業者名:</th>
-              <td>{QRData[0][0]}</td>
-            </tr>
-            <tr>
-              <th scope='row'>商品コード:</th>
-              <td>{QRData[0][1]}</td>
-            </tr>
-            <tr>
-              <th scope='row'>商品名:</th>
-              <td>{QRData[0][2]}</td>
-            </tr>
-            <tr>
-              <th scope='row'>システム在庫数:</th>
-              <td>{QRData[0][6]}</td>
-            </tr>
-          </tbody>
-        </table>
-        </div>
-        <div className="control-area">
-          <input
-            type="tel"
-            pattern='^[0-9]+$'
-            placeholder='現物数'
-            value={InsertNumber}
-            onChange={(e) => numberchange(e)}
-          />
-          <a className="buttonUnderline" type="button" onClick={NextScan}>
-            次へ
-          </a>
-        </div>
+    <div className="window-area">
+      <div className="set">
+        <div className="detail">
+          <table>
+            <tbody>
+              <tr>
+                <th scope='row'>業者名:</th>
+                <td>{QRData[0][0]}</td>
+              </tr>
+              <tr>
+                <th scope='row'>商品コード:</th>
+                <td>{QRData[0][1]}</td>
+              </tr>
+              <tr>
+                <th scope='row'>商品名:</th>
+                <td>{QRData[0][2]}</td>
+              </tr>
+              <tr>
+                <th scope='row'>システム在庫数:</th>
+                <td>{QRData[0][6]}</td>
+              </tr>
+            </tbody>
+          </table>
+          </div>
+          <div className="control-area">
+            <input
+              type="tel"
+              pattern='^[0-9]+$'
+              placeholder='現物数'
+              value={InsertNumber}
+              onChange={(e) => numberchange(e)}
+            />
+            <a className="buttonUnderline" type="button" onClick={NextScan}>
+              次へ
+            </a>
+          </div>
+      </div>
     </div>
   );
 }
